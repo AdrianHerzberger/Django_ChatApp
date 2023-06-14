1d2b75e26dc70b90e1ae4d94e4c9389cc11566de
Adrian Herzberger <adrianherzberger94@gmail.com>
2023-06-14 15:43:55 +0200

    first commit

--- django_chat_app/urls.py
+++ django_chat_app/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
 from django.contrib import admin
 from django.urls import path
 
@@ -25,7 +26,7 @@ def trigger_error(request):
     division_by_zero = 1 / 0
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('chat/', index),
     path('login/', login_view),
